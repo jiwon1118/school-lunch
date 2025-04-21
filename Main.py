4644990391c736292410224e39224d067cd66850
jiwon1118 <b23386585@gmail.com>
2025-04-21 10:06:00 +0900

    pages

---
+++ Main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.set_page_config(page_title="Main", page_icon="🏠")
+st.title("School-lunch 🍱")
+#st.image
+
+st.write("🏫 **전국 학교 급식 정보를 한눈에!**")
+
+st.write(" ")
+st.write(" ")
+st.write("**📅 급식 일정 확인**")
+st.write(" ")
+st.write("**📊 학교별 급식 통계와 트렌드, 메뉴 인기 순위**")
+st.write(" ")
+st.write("**🧪영양 불균형 탐지, 급식 품질 분석**")
+st.write(" ")
+st.write(" ")
+
+
+
+
+st.sidebar.markdown("# 배백조조🦢")
+
+#st.sidebar.title("🔍 페이지 선택")
+#page = st.sidebar.radio("이동할 페이지를 선택하세요", ["🏠 Main", "🧐 Products Recommend", "📅 Schedule", "✔ Outcall", "🎵 Music"])
+
+
+
+
+st.markdown("---")
+st.write("📌 **Made with   using Streamlit** | © 2025 School-lunch")
